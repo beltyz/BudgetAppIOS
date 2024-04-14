dd66e353c8c5a4e56ef89a939840adee3ec8d043
beltyz <Lushchyk321@gmail.com>
2024-04-14 15:20:55 +0200

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Dima on 14.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
