f220d3c6972da205a7e1ae868f9c27814f01f141
beltyz <Lushchyk321@gmail.com>
2024-04-14 17:07:58 +0200

    create design

--- BudgetApp/ContentView.swift
+++ BudgetApp/ContentView.swift
@@ -9,13 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("bg")
+                .resizable()
+            .ignoresSafeArea()
+            ScrollView {
+                VStack(alignment: .center,spacing: 40) {
+                    HStack {
+                        Text("Budget App")
+                            .bold()
+                            .font(.title)
+                            .foregroundStyle(LinearGradient(colors: [.gradient1,.gradient2], startPoint: .leading, endPoint: .trailing))
+                            .padding()
+                        Spacer()
+                        Button(action: {}) {
+                               Text("Sign in")
+                                 .padding()
+                                 .foregroundColor(.white)
+                                 .background(.deepGreen)
+                                 .cornerRadius(10)
+                             }
+                             .shadow(color: .greenShadow, radius: 15, y: 5)
+                    }
+                    
+                    HStack {
+                        Text("Welcome to the BudTracker")
+                            .bold()
+                            .dynamicTypeSize(.medium)
+                            .foregroundStyle(LinearGradient(colors: [.gradient1,.gradient2], startPoint: .leading, endPoint: .trailing))
+                       
+                        Image("walletWithCash")
+                            .resizable()
+                            .frame(width: 200,height: 200)
+                            
+                        
+                    }
+                    Text("Join us today and transform the way you manage your money. Take the first step towards a financially secure future with BudTracker! \n\nTake control of your finances with BudTracker, your ultimate financial companion. Navigate your financial journey with ease using our smart, intuitive, and user-friendly tool.")
+                        .multilineTextAlignment(.center)
+                        .dynamicTypeSize(.xLarge)
+                        .foregroundStyle(LinearGradient(colors: [.gradient1,.gradient2], startPoint: .leading, endPoint: .trailing))
+                    
+                        
+                    
+                    Text("Benefits of Tracking your Budget")
+                        .font(.system(size: 20))
+                        .bold()
+                        .foregroundStyle(LinearGradient(colors: [.gradient1,.gradient2], startPoint: .leading, endPoint: .trailing))
+                        
+                        .padding()
+                    
+                    Spacer()
+                    
+                    
+                    
+                }
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
